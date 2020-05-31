c8ca5f4586b8ae2dc6ab097ce724f3c7e3ca381a
Israel Berlanga <israel0688@hotmail.com>
2020-05-30 19:06:44 -0500

    successfully GET and POST data using insomnia

--- server.js
+++ server.js
@@ -5,10 +5,50 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const { notes } = require('./Develop/db/db.json');
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
+
+app.post('/api/notes', (req, res) => {
+    //set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        // add note to json file and animals array in this function
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+
+    return note;
+}
+
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
+}
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
